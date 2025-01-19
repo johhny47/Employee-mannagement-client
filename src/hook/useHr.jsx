b9231c58f21f8102de321ae82c094a567ff14693
johhny47 <mdjohnny063@gmail.com>
2025-01-19 09:51:59 +0600

    add useHr

--- src/hook/useHr.jsx
+++ src/hook/useHr.jsx
@@ -1,1 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react"; 
+import AuthContext from "../provider/AuthContext";  
+import useAxiosSecure from "./useAxiosSecure";
 
+const useHr = () => {
+
+  const { user } = useContext(AuthContext);
+  
+  
+  const axiosSecure = useAxiosSecure();
+  
+ 
+  const { data: isHr, isLoading: isHrLoading } = useQuery(
+    {
+      queryKey: [user?.email, 'isHr'],
+      queryFn: async () => {
+       const res  = await axiosSecure.get(`/hrCheck/${user?.email}`);
+        console.log(res.data);  
+        return res.data?.Hr;  
+      },
+    
+    }
+  );
+
+  return [isHr, isHrLoading];
+};
+
+export default useHr;
