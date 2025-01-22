c54dbc2fb88126c5179e0f4e2ca9c5519bfc98d9
johhny47 <mdjohnny063@gmail.com>
2025-01-22 12:09:09 +0600

    add testimonial

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import useAdmin from "../hook/useAdmin";
 import Banner from "../component/HomeComponent/Banner/Banner";
 import Service from "../component/HomeComponent/Service/Service";
+import Testimonial from "../component/HomeComponent/Testimonial/Testimonial";
 
 
 
@@ -17,6 +18,11 @@ const Home = () => {
         <div>
            <Banner></Banner>
            <Service></Service>
+           <div className="my-14">
+            <h1 className="text-center font bold text-2xl md:text-3xl lg:text-4xl  my-14 px-10">Testimonial</h1>
+           <Testimonial></Testimonial>
+           </div>
+          
            
         </div>
     );
