4d585a1c06383333977d2d633874b2a2321dc7f4
johhny47 <mdjohnny063@gmail.com>
2025-02-18 18:59:01 +0600

    add banner

--- src/component/HomeComponent/Banner/Banner.jsx
+++ src/component/HomeComponent/Banner/Banner.jsx
@@ -1,13 +1,40 @@
-
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from "swiper/modules"; 
+import 'swiper/css';
+import 'swiper/swiper-bundle.css';
+import { motion } from 'framer-motion';
 import banner1 from "../../../assets/BannerImg2.avif"
 
 const Banner = () => {
-
   return (
-    <div className='h-[450px] md:h-[600px] lg:h-[600px]'>
-      <img src={banner1} alt="" className="h-full min-w-full" />
-    
+    <div className="mt-16 ">
+      <Swiper
+        modules={[Autoplay]}  
+        spaceBetween={50}
+        slidesPerView={1}
+        autoplay={{
+          delay: 2000,  
+          disableOnInteraction: false,  
+        }}
+      >
+        <SwiperSlide>
+          <motion.img
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.6 }}
+            src={banner1}
+            alt="Banner 1"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner1} alt="Banner 2" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner1} alt="Banner 4" />
+        </SwiperSlide>
+       
+       
+      </Swiper>
     </div>
   );
 };
